34203806f4bb8fc2f83ff7744356218be0116a8c
Gaurav Kashyap <gaurav783037@gmail.com>
2025-04-29 15:50:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import joblib
+
+
+model=joblib.load('placement.pkl')
+
+def main():
+    st.title('welcome to predictore ')
+    cgpa=st.number_input('enter your cgpa ')
+    cgpa=st.slider('choose your packge from slider',min_value=0.0,max_value=10.0,step=0.1)
+    st.write('entered cgpa',cgpa)
+
+    if st.button('prdict'):
+        result=model.predict([[cgpa]])
+        st.success(f'your package would be{result}')
+
+if __name__=='__main__':
+    main()
+        
