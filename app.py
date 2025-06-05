9f5438cdfdbfd678148909cdd93496f1cf20285c
Gaurav Kashyap <gaurav783037@gmail.com>
2025-06-05 15:04:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import streamlit as st
 import joblib
+import numpy as np
 
 
-model=joblib.load('placement.pkl')
+model=joblib.load("placement_model.pkl")
 
-def main():
-    st.title('welcome to predictore ')
-    cgpa=st.number_input('enter your cgpa ')
-    cgpa=st.slider('choose your cgpa from slider',min_value=0.0,max_value=10.0,step=0.1)
-    st.write('entered cgpa',cgpa)
+st.title("Placement Package Predictor")
+st.write("Fill in the details to see your estimated placement package (in LPA)")
 
-    if st.button('prdict'):
-        result=model.predict([[cgpa]])
-        st.success(f'your package would be{result} Lpa')
 
-if __name__=='__main__':
-    main()
-        
+cgpa= st.slider("Your CGPA", 5.0, 10.0, step=0.1)
+internship= st.slider("Internship Rating (1-10)", 1, 10)
+experience= st.slider("Years of Experience", 0, 5)
+coding= st.slider("Coding Skill (1-10)", 1, 10)
+communication= st.slider("Communication Skill (1-10)", 1, 10)
+
+
+if st.button("Predict"):
+    input_data = np.array([[cgpa, internship, experience, coding, communication]])
+    result = model.predict(input_data)[0]
+    st.write("Your predicted package is:")
+    st.success(str(round(result, 2)) + " LPA")
