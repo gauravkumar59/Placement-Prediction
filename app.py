5dbb7089ef02d1758e9b4b46f8bf6ab5ba0ce46b
Gaurav Kashyap <gaurav783037@gmail.com>
2025-06-05 14:49:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ model=joblib.load('placement.pkl')
 def main():
     st.title('welcome to predictore ')
     cgpa=st.number_input('enter your cgpa ')
-    cgpa=st.slider('choose your packge from slider',min_value=0.0,max_value=10.0,step=0.1)
+    cgpa=st.slider('choose your cgpa from slider',min_value=0.0,max_value=10.0,step=0.1)
     st.write('entered cgpa',cgpa)
 
     if st.button('prdict'):
         result=model.predict([[cgpa]])
-        st.success(f'your package would be{result}')
+        st.success(f'your package would be{result} Lpa')
 
 if __name__=='__main__':
     main()
